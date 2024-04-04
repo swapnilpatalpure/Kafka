089aabd129ffbdbec949925b7c8800b75f6084fe
swapnilpatalpure <swapnil24patalpure@gmai>
2024-04-05 01:46:14 +0530

    adding deliveryboy & enduser services

---
+++ deliveryboy/src/main/java/com/deliveryboy/service/KafkaService.java
@@ -0,0 +1,27 @@
+package com.deliveryboy.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.deliveryboy.config.AppConstants;
+
+@Service
+public class KafkaService {
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	private Logger logger= LoggerFactory.getLogger(KafkaService.class);
+
+	public boolean updateLoccation(String location) {
+
+		this.kafkaTemplate.send(AppConstants.LOCATION_TOPIC_NAME, location);
+		this.logger.info("Message Produced");
+		return true;
+
+	}
+
+}
